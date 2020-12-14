6b79d375c87157979bf2c8fbd2b64eaf05a92331
Marek Musiał <mmusialmarek@gmail.com>
2020-12-14 11:03:48 +0100

    created test for HomeController

---
+++ src/test/java/it/musialmarek/tacocloud/home/HomeControllerTest.java
@@ -0,0 +1,27 @@
+package it.musialmarek.tacocloud.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.hamcrest.Matchers.containsString;
+
+
+@WebMvcTest
+class HomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldOpenViewWithNameHomeWithWelcomingInside() throws Exception {
+        //when
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(
+                        containsString("Witaj w...")));
+    }
+}
