0803acece06a97524df70196fedca88c017c2cd9
Marek Musiał <mmusialmarek@gmail.com>
2020-12-28 14:38:15 +0100

    replaced HomeControllerTest to web package

--- src/test/java/it/musialmarek/tacocloud/web/home/HomeControllerTest.java
+++ src/test/java/it/musialmarek/tacocloud/web/home/HomeControllerTest.java
@@ -1,4 +1,4 @@
-package it.musialmarek.tacocloud.home;
+package it.musialmarek.tacocloud.web.home;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
