5d2d897f0ec5fe0578da7221aec621aeccf7a897
Marek Musiał <mmusialmarek@gmail.com>
2021-01-14 14:28:54 +0100

    added annotations for rules of taco validation

--- src/main/java/it/musialmarek/tacocloud/model/Taco.java
+++ src/main/java/it/musialmarek/tacocloud/model/Taco.java
@@ -2,7 +2,10 @@ package it.musialmarek.tacocloud.model;
 
 import lombok.*;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
+
 
 @Getter
 @Setter
@@ -11,7 +14,9 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Taco {
-
+    @NotNull
+    @Size(min = 5, message = "the name should contain at least 5 characters")
     private String name;
+    @Size(min=1, message = "You have to take at least one ingredient")
     private List<Ingredient> ingredients;
 }
