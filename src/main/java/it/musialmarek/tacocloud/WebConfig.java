58358e08b4f16a49b0dde88dc620dab53ddda9b4
Marek Musiał <mmusialmarek@gmail.com>
2021-01-14 14:09:54 +0100

    added converter to registry

---
+++ src/main/java/it/musialmarek/tacocloud/WebConfig.java
@@ -0,0 +1,19 @@
+package it.musialmarek.tacocloud;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(ingredientsConverter());
+    }
+
+    @Bean
+    public IngredientConverter ingredientsConverter() {
+        return new IngredientConverter();
+    }
+}
