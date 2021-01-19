769e25d19f3d62e22d02adedca4fb1780b5fd6c7
Marek Musiał <mmusialmarek@gmail.com>
2021-01-19 18:01:43 +0100

    added view controller for home

--- src/main/java/it/musialmarek/tacocloud/WebConfig.java
+++ src/main/java/it/musialmarek/tacocloud/WebConfig.java
@@ -3,6 +3,7 @@ package it.musialmarek.tacocloud;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,5 +16,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public IngredientConverter ingredientsConverter() {
         return new IngredientConverter();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
     }
 }
